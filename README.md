a65657f6e41ec2875d62a0fe4b276b844cb2fc4d
unknown <sidkamalapuri@gmail.com>
2020-11-27 14:43:34 +0530

    First Commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# amarkamalapuri.github.io
+Hello, welcome to my website. [amarkamalapuri.github.io](https://amarkamalapuri.github.io)
+
+Created with the help of [Bootstrap](http://getbootstrap.com)
+
+If you want to give me your own suggestions email me on `sidkamalapuri@gmail.com`
---
+++ structuredata.js
@@ -0,0 +1,24 @@
+<script type="application/ld+json">
+{
+  "@context": "http://schema.org",
+  "@type": "Person",
+  "familyName": "Kamalapuri",
+  "givenName": "Amar",
+  "url": "https://amarkamalapuri.github.io",
+  "email": "sidkamalapuri@gmail.com",
+  "affiliation": "National Institute Of Technology, Silchar, Assam",
+  "birthDate": "17 Aug 2000",
+  "gender": "http://schema.org/Male",
+  "hasOccupation": "Full Stack Developer",
+  "nationality": "Indian",
+  "image": "https://avatars1.githubusercontent.com/u/32615499?s=460&v=4",
+  
+ 
+  "sameAs": [
+    "https://twitter.com/Sid_Kamalapuri",
+    "https://www.instagram.com/amar_kamalapuri/",
+    "https://www.linkedin.com/in/amar-kamalapuri-9b76581a0/"
+    "https://github.com/AmarKamalapuri"
+    ]
+}
+</script>
